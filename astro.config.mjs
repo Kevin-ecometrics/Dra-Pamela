decdd3e623149e5d9915fb1c8132d1696cd3afdb
Superuser <kevin@e-commetrics.com>
2024-02-20 09:44:18 -0800

    update version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import metaTags from "astro-meta-tags";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "http://www.doctorapamelaperez.com/",
+  integrations: [tailwind(), metaTags(), partytown(), sitemap(), robotsTxt(), react()]
 });
